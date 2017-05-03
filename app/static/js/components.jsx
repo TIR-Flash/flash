5fc5f06829a953dc48ccbe30d0bf1e8cf4cc18ff
momopranto <m.amin@nyu.edu>
2017-05-03 00:00:35 -0400

    derp

--- app/static/js/components.jsx
+++ app/static/js/components.jsx
@@ -25,7 +25,7 @@ class YT extends React.Component {
 }
 
 // ReactDOM.render(<YT href="https://www.youtube.com/watch?v=A71aqufiNtQ"/>, document.getElementById('video'));
-$('#video').each(function(){
+$('.video').each(function(){
   console.log(this);
   ReactDOM.render(<YT href={$(this).attr('link')} />, this);
 });
