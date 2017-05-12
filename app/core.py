55d4ae6b20d683f8d932e3a942fe0ee0e5bb92e4
EC2 Default User <ec2-user@ip-172-31-76-64.ec2.internal>
2017-05-12 20:50:36 +0000

    better

--- app/core.py
+++ app/core.py
@@ -57,6 +57,10 @@ ReactDOM.render(
     lid = mongo.db.lessons.insert_one(l).inserted_id
     return jsonify(str(l))
 
+@core.route('/paid')
+def paid():
+    return render_template('home.html')
+
 @core.route('/')
 def home():
     l = {
@@ -77,27 +81,97 @@ def home():
 		{
 		    "title": "Getting Started",
 		    "type": "code",
-		    "description": "First, you'll need to install the dependencies.",
+		    "description": "First, you'll need to install the dependencies. React is built on node.js so you will need the node package manager (npm) in order to install modules. ",
 		    "content": '''apt install npm
-npm init
-npm install --save react react-dom'''
+npm init'''
 		},
+                {
+                    "title": "Configure webpack",
+                    "type": "code",
+                    "description": "You will need a file called webpack.config.js that tells webpack which files to bundle and how.",
+                    "content": '''var path = require('path');
+
+module.exports = {
+  entry: path.join(__dirname, 'app/static/js/hello.jsx'),
+  output: {
+    filename: 'bundle.js',
+    path: path.join(__dirname, 'app/static/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      }
+    ]
+  }
+};
+'''
+                },
+                {
+                    "title": "Configuring your app",
+                    "type": "code",
+                    "description": "You will also need a file called package.json that does a lot of things such as installing dependencies and buiding your app. You will also need a bundler that lets you package all your code together. We will use webpack but you can also use browserify. Finally, you will need a compiler such as Babel to make your ES6 (the latest standard of javascript) code compatible with older browsers.",
+                    "content": '''{
+  "name": "myappp",
+  "version": "1.0.0",
+  "description": "My first react app",
+  "dependencies": {
+    "webpack": "latest", 
+    "webpack-dev-server": "latest", 
+    "babel-cli": "latest", 
+    "babel-core": "latest", 
+    "babel-loader": "latest", 
+    "babel-preset-env": "latest", 
+    "babel-preset-es2015": "latest", 
+    "babel-preset-react": "latest",
+    "react": "latest", 
+    "react-dom": "latest", 
+  },
+  "scripts": {
+    "build": "node_modules/.bin/webpack --config webpack.config.js"
+  },
+  "babel": {
+    "presets": ["env", "es2015", "react"]
+  }
+}
+'''
+                },
 		{
 		    "title": "Exercise 1: Hello World!",
 		    "type": "code",
-		    "description": "Try this out",
+		    "description": "Create a file called hello.jsx where you will add the following code.",
 		    "content": '''import React from 'react';
 import ReactDOM from 'react-dom';
 
-ReactDOM.render(
-  &lt;h1&gt;Hello, world!&lt;/h1&gt;,
-  document.getElementById('root')
-);'''
-		}
+class Welcome extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name}</h1>;
+  }
+}
+
+ReactDOM.render(<Welcome name="your-name-here"/>, document.getElementById('root'));'''
+		},
+                {
+                    "title": "Run your app",
+                    "type": "code",
+                    "description": "Now all you need is an html file where you will use your react component. Create a file called index.html and add the following code and then you can just run 'npm install', 'npm run build'. Open your index.html file in a browser and your app should be running.",
+                    "content": '''<!DOCTYPE html>
+<html>
+<head>
+    <title>My first React.js app</title>
+</head>
+<body>
+    <div id="root"></div>
+    <script src="bundle.js"></script>
+</body>
+</html>'''
+                }
 	    ]
 	}
     lid = mongo.db.lessons.insert_one(l).inserted_id
-    return render_template('home.html')
+    return render_template('free.html')
 
 @core.route('/lesson/<topic>')
 def lesson(topic):
